cb5ab6d37de5de7a57ee32898cae4a41751b8988
Neeraja <152985436+Neeraja2121@users.noreply.github.com>
2026-05-16 17:26:42 +0530

    Update API_Mocking.spec.js

--- tests/API_Mocking.spec.js
+++ tests/API_Mocking.spec.js
@@ -1,3 +1,5 @@
  import{test,expect} from '@playwright/test'
 
- test('API Mocking',async({route}))
+ test('API Mocking',async({page})=>{
+  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+  );
